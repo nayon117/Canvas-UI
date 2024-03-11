75efa5d67c31e1668ad9ad1f789153261cabc343
nayon117 <hasibul.nayon1@gmail.com>
2024-03-11 16:43:08 +0600

    update notfound page

--- src/Pages/NotFound/NotFound.jsx
+++ src/Pages/NotFound/NotFound.jsx
@@ -1,4 +1,3 @@
-import { Button } from "@material-tailwind/react";
 import { Link } from "react-router-dom";
 import Navbar from "../../components/Navbar";
 
@@ -8,17 +7,17 @@ const NotFound = () => {
       <Navbar />
       <div className="w-full mt-20 ">
         <div className="py-10 mx-5 lg:mx-0">
-          <div className="md:w-3/4 w-full flex flex-col items-center justify-center mx-auto px-3 lg:px-0  backdrop-blur-md bg-white/10 py-10 rounded-xl shadow-lg shadow-cyan-600">
-            <h1 className="text-white text-4xl md:text-5xl lg:text-7xl text-center logo-text tracking-wide h-full font-bold lg:w-3/4 mx-auto">
+          <div className="md:w-3/4 w-full flex flex-col items-center justify-center mx-auto px-3 lg:px-0  backdrop-blur-md bg-white/10 py-10 rounded-xl shadow-lg shadow-[#0072ff]">
+            <h1 className=" text-4xl md:text-5xl lg:text-7xl text-center logo-text tracking-wide h-full font-bold lg:w-3/4 mx-auto">
               Thank You for Your Support! Exciting Updates Coming Soon
             </h1>
             <Link to={"components/avatar"}>
-              <Button
-                className="mx-auto flex justify-center my-10"
+              <button
+                className="mx-auto btn flex justify-center my-10"
                 color="blue"
               >
                 Explore Our Components
-              </Button>
+              </button>
             </Link>
           </div>
         </div>
