db28fcfa96102392a321b2515a2737de60bdb0b0
nayon117 <hasibul.nayon1@gmail.com>
2024-03-13 16:38:05 +0600

    create badge2

--- src/components/App/Badges/Badge2.jsx
+++ src/components/App/Badges/Badge2.jsx
@@ -1,9 +1,20 @@
+import CodeBox from "../../../Shared/CodeBox/CodeBox";
 
 const Badge2 = () => {
-    return(
-        <div>
-             <p> Welcome to Badge2 part </p>
-        </div>
-    )
-}
+  return (
+   <CodeBox stringCode={`<button type="button" className="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Messages<span className="inline-flex items-center justify-center w-4 h-4 ms-2 text-xs font-semibold text-blue-800 bg-blue-200 rounded-full">2</span></button>`} text={`Button with Badge`}>
+     <div className="flex items-center justify-center gap-4 my-3">
+      <button
+        type="button"
+        className="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        Messages
+        <span className="inline-flex items-center justify-center w-4 h-4 ms-2 text-xs font-semibold text-blue-800 bg-blue-200 rounded-full">
+          2
+        </span>
+      </button>
+    </div>
+   </CodeBox>
+  );
+};
 export default Badge2;
