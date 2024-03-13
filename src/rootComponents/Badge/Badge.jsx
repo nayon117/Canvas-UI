e97091d7de524cca8dddc17ee7efeb6cce7586b5
nayon117 <hasibul.nayon1@gmail.com>
2024-03-13 16:51:46 +0600

    create badge 3

--- src/rootComponents/Badge/Badge.jsx
+++ src/rootComponents/Badge/Badge.jsx
@@ -1,5 +1,6 @@
 import Badge1 from "../../components/App/Badges/Badge1";
 import Badge2 from "../../components/App/Badges/Badge2";
+import Badge3 from "../../components/App/Badges/Badge3";
 
  
 const Badge = () => {
@@ -7,6 +8,7 @@ const Badge = () => {
         <div>
            <Badge1/>
            <Badge2/>
+           <Badge3/>
         </div>
     )
 }
