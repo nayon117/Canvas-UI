48e3d99652315058d38eb88fa159ed4463aa56d5
nayon117 <hasibul.nayon1@gmail.com>
2024-03-13 16:21:24 +0600

    create alert3

--- src/rootComponents/Alert/Alert.jsx
+++ src/rootComponents/Alert/Alert.jsx
@@ -1,11 +1,13 @@
 import Alert1 from "../../components/App/Alerts/Alert1";
 import Alert2 from "../../components/App/Alerts/Alert2";
+import Alert3 from "../../components/App/Alerts/Alert3";
 
 const Alert = () => {
   return (
     <div>
       <Alert1 />
       <Alert2/>
+      <Alert3/>
     </div>
   );
 };
