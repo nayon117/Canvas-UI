723be25461635ed0bb4bcacc0ca7e8c85c99a748
nayon117 <hasibul.nayon1@gmail.com>
2024-03-15 07:13:24 +0600

    add card 4

--- src/rootComponents/Card/Card.jsx
+++ src/rootComponents/Card/Card.jsx
@@ -1,6 +1,7 @@
 import Card1 from "../../components/App/Cards/Card1";
 import Card2 from "../../components/App/Cards/Card2";
 import Card3 from "../../components/App/Cards/Card3";
+import Card4 from "../../components/App/Cards/Card4";
 
 const Card = () => {
   return (
@@ -8,6 +9,7 @@ const Card = () => {
       <Card1 />
       <Card2/>
       <Card3/>
+      <Card4/>
     </div>
   );
 };
