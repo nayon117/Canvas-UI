483adcc46bd07561e012c22dedd90b4d04340dab
nayon117 <hasibul.nayon1@gmail.com>
2024-03-18 13:05:42 +0600

    add alert5

--- src/rootComponents/Alert/Alert.jsx
+++ src/rootComponents/Alert/Alert.jsx
@@ -2,6 +2,7 @@ import Alert1 from "../../components/App/Alerts/Alert1";
 import Alert2 from "../../components/App/Alerts/Alert2";
 import Alert3 from "../../components/App/Alerts/Alert3";
 import Alert4 from "../../components/App/Alerts/Alert4";
+import Alert5 from "../../components/App/Alerts/Alert5";
 
 const Alert = () => {
   return (
@@ -10,6 +11,7 @@ const Alert = () => {
       <Alert2/>
       <Alert3/>
       <Alert4/>
+      <Alert5/>
     </div>
   );
 };
