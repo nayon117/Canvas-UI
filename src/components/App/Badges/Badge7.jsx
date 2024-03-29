c12e202e16214c9a11522259c1a3f8cbb6a014bc
nayon117 <hasibul.nayon1@gmail.com>
2024-03-29 11:02:22 +0600

    add badge7

---
+++ src/components/App/Badges/Badge7.jsx
@@ -0,0 +1,24 @@
+import CodeBox from "../../../Shared/CodeBox/CodeBox";
+
+const Badge7 = () => {
+  return (
+    <CodeBox stringCode={`<div className="w-max flex items-center bg-gray-900 shadow-lg rounded-full font-[sans-serif] mx-auto">
+    <span className="bg-yellow-300 text-gray-900 text-base font-semibold rounded-full w-16 h-16 flex items-center justify-center">
+      70%
+    </span>
+    <h3 className="text-white text-lg font-semibold uppercase mx-4">
+      Badge design
+    </h3>
+  </div>`} text={`Badge`}>
+      <div className="w-max flex items-center bg-gray-900 shadow-lg rounded-full font-[sans-serif] mx-auto">
+        <span className="bg-yellow-300 text-gray-900 text-base font-semibold rounded-full w-16 h-16 flex items-center justify-center">
+          70%
+        </span>
+        <h3 className="text-white text-lg font-semibold uppercase mx-4">
+          Badge design
+        </h3>
+      </div>
+    </CodeBox>
+  );
+};
+export default Badge7;
