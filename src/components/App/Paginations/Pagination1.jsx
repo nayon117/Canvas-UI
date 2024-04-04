bce418354b1e3174cc7ce1943973ae37bf341034
nayon117 <hasibul.nayon1@gmail.com>
2024-04-04 21:44:24 +0600

    add pagination1

---
+++ src/components/App/Paginations/Pagination1.jsx
@@ -0,0 +1,84 @@
+import CodeBox from "../../../Shared/CodeBox/CodeBox";
+
+const Pagination1 = () => {
+  return (
+    <CodeBox stringCode={`<ul className="flex space-x-4 justify-center">
+    <li className="flex items-center justify-center shrink-0 bg-gray-300 w-10 h-10 rounded-lg">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="w-4 fill-gray-400"
+        viewBox="0 0 55.753 55.753"
+      >
+        <path
+          d="M12.745 23.915c.283-.282.59-.52.913-.727L35.266 1.581a5.4 5.4 0 0 1 7.637 7.638L24.294 27.828l18.705 18.706a5.4 5.4 0 0 1-7.636 7.637L13.658 32.464a5.367 5.367 0 0 1-.913-.727 5.367 5.367 0 0 1-1.572-3.911 5.369 5.369 0 0 1 1.572-3.911z"
+          data-original="#000000"
+        />
+      </svg>
+    </li>
+    <li className="flex items-center justify-center shrink-0 bg-[#1C372C]  border-2 border-[#1C372C] cursor-pointer text-base font-bold text-white w-10 h-10 rounded-lg">
+      1
+    </li>
+    <li className="flex items-center justify-center shrink-0 hover:bg-gray-50  border-2 cursor-pointer text-base font-bold text-[#333] w-10 h-10 rounded-lg">
+      2
+    </li>
+    <li className="flex items-center justify-center shrink-0 hover:bg-gray-50  border-2 cursor-pointer text-base font-bold text-[#333] w-10 h-10 rounded-lg">
+      3
+    </li>
+    <li className="flex items-center justify-center shrink-0 hover:bg-gray-50  border-2 cursor-pointer text-base font-bold text-[#333] w-10 h-10 rounded-lg">
+      4
+    </li>
+    <li className="flex items-center justify-center shrink-0 hover:bg-gray-50 border-2 cursor-pointer w-10 h-10 rounded-lg">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="w-4 fill-gray-400 rotate-180"
+        viewBox="0 0 55.753 55.753"
+      >
+        <path
+          d="M12.745 23.915c.283-.282.59-.52.913-.727L35.266 1.581a5.4 5.4 0 0 1 7.637 7.638L24.294 27.828l18.705 18.706a5.4 5.4 0 0 1-7.636 7.637L13.658 32.464a5.367 5.367 0 0 1-.913-.727 5.367 5.367 0 0 1-1.572-3.911 5.369 5.369 0 0 1 1.572-3.911z"
+          data-original="#000000"
+        />
+      </svg>
+    </li>
+  </ul>`} text={`pagination UI`}>
+      <ul className="flex space-x-4 justify-center">
+        <li className="flex items-center justify-center shrink-0 bg-gray-300 w-10 h-10 rounded-lg">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-4 fill-gray-400"
+            viewBox="0 0 55.753 55.753"
+          >
+            <path
+              d="M12.745 23.915c.283-.282.59-.52.913-.727L35.266 1.581a5.4 5.4 0 0 1 7.637 7.638L24.294 27.828l18.705 18.706a5.4 5.4 0 0 1-7.636 7.637L13.658 32.464a5.367 5.367 0 0 1-.913-.727 5.367 5.367 0 0 1-1.572-3.911 5.369 5.369 0 0 1 1.572-3.911z"
+              data-original="#000000"
+            />
+          </svg>
+        </li>
+        <li className="flex items-center justify-center shrink-0 bg-[#1C372C]  border-2 border-[#1C372C] cursor-pointer text-base font-bold text-white w-10 h-10 rounded-lg">
+          1
+        </li>
+        <li className="flex items-center justify-center shrink-0 hover:bg-gray-50  border-2 cursor-pointer text-base font-bold text-[#333] w-10 h-10 rounded-lg">
+          2
+        </li>
+        <li className="flex items-center justify-center shrink-0 hover:bg-gray-50  border-2 cursor-pointer text-base font-bold text-[#333] w-10 h-10 rounded-lg">
+          3
+        </li>
+        <li className="flex items-center justify-center shrink-0 hover:bg-gray-50  border-2 cursor-pointer text-base font-bold text-[#333] w-10 h-10 rounded-lg">
+          4
+        </li>
+        <li className="flex items-center justify-center shrink-0 hover:bg-gray-50 border-2 cursor-pointer w-10 h-10 rounded-lg">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-4 fill-gray-400 rotate-180"
+            viewBox="0 0 55.753 55.753"
+          >
+            <path
+              d="M12.745 23.915c.283-.282.59-.52.913-.727L35.266 1.581a5.4 5.4 0 0 1 7.637 7.638L24.294 27.828l18.705 18.706a5.4 5.4 0 0 1-7.636 7.637L13.658 32.464a5.367 5.367 0 0 1-.913-.727 5.367 5.367 0 0 1-1.572-3.911 5.369 5.369 0 0 1 1.572-3.911z"
+              data-original="#000000"
+            />
+          </svg>
+        </li>
+      </ul>
+    </CodeBox>
+  );
+};
+export default Pagination1;
