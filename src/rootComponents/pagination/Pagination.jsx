5670d2c7ebb4174292ef0d316dda11e4c9e5a8af
nayon117 <hasibul.nayon1@gmail.com>
2024-04-04 21:47:53 +0600

    add pagination2

--- src/rootComponents/pagination/Pagination.jsx
+++ src/rootComponents/pagination/Pagination.jsx
@@ -1,9 +1,11 @@
 import Pagination1 from "../../components/App/Paginations/Pagination1";
+import Pagination2 from "../../components/App/Paginations/Pagination2";
 
 const Pagination = () => {
     return(
         <div>
              <Pagination1/>
+             <Pagination2/>
         </div>
     )
 }
