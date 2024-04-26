04298fbfe5f2d6acc6d4de59834ac46a98147ab3
nayon117 <hasibul.nayon1@gmail.com>
2024-04-26 09:16:04 +0600

    add form5

--- src/rootComponents/Form/Form.jsx
+++ src/rootComponents/Form/Form.jsx
@@ -2,6 +2,7 @@ import Form1 from "../../components/App/Form/Form1";
 import Form2 from "../../components/App/Form/Form2";
 import Form3 from "../../components/App/Form/Form3";
 import Form4 from "../../components/App/Form/Form4";
+import Form5 from "../../components/App/Form/Form5";
 
 const Form = () => {
     return (
@@ -10,6 +11,7 @@ const Form = () => {
             <Form2 />
             <Form3 />
             <Form4 />
+            <Form5/>
         </div>
     )
 }
