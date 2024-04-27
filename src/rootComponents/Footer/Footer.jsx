0c3a3c3e4d2b11f2fb4a0a0aa10911120e04fafb
nayon117 <hasibul.nayon1@gmail.com>
2024-04-27 09:14:57 +0600

    add footer3

--- src/rootComponents/Footer/Footer.jsx
+++ src/rootComponents/Footer/Footer.jsx
@@ -1,11 +1,13 @@
 import Footer1 from "../../components/App/Footers/Footer1";
 import Footer2 from "../../components/App/Footers/Footer2";
+import Footer3 from "../../components/App/Footers/Footer3";
 
 const Footer = () => {
     return(
         <div>
               <Footer1/>
               <Footer2/>
+              <Footer3/>
         </div>
     )
 }
